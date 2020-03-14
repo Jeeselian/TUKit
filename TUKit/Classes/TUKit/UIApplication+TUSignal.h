0914362444055f3e84af601120871f225b4121fc
GFB <GFB@GFBdeMac-mini.local>
2020-03-14 13:55:13 +0800

    init

---
+++ TUKit/Classes/TUKit/UIApplication+TUSignal.h
@@ -0,0 +1,20 @@
+//
+//  UIApplication+TUSignal.h
+//  TUTest
+//
+//  Created by Jason on 2020/3/13.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+//#import <AppKit/AppKit.h>
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIApplication (TUSignal)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TUKit/Classes/TUKit/UIApplication+TUSignal.m
@@ -0,0 +1,16 @@
+//
+//  UIApplication+TUSignal.m
+//  TUTest
+//
+//  Created by Jason on 2020/3/13.
+//  Copyright © 2020 Jason. All rights reserved.
+//   
+
+#import "UIApplication+TUSignal.h"
+
+//#import <AppKit/AppKit.h>
+
+
+@implementation UIApplication (TUSignal)
+
+@end
